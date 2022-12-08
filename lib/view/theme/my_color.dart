fe5f799d98ffe471d77e95666180854c91274415
おりょう <oryo.developer@gmail.com>
2022-12-09 08:20:55 +0900

    色を作成

---
+++ lib/view/theme/my_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static const black = Colors.black;
+  static const white = Colors.white;
+  static const amber = Colors.amber;
+  static const transparent = Colors.transparent;
+}
