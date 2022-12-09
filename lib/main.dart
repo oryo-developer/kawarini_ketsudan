7747947a34a063e99b4d1f04904b620beb1668e6
おりょう <oryo.developer@gmail.com>
2022-12-10 02:48:40 +0900

    ProviderScopeで囲んだ

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kawarini_ketsudan/view/page/home_page.dart';
 import 'package:kawarini_ketsudan/view/theme/my_theme.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
