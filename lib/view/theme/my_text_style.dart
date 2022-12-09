8b536ba8c2db89d32e180b97d9e07b775edd333d
おりょう <oryo.developer@gmail.com>
2022-12-10 02:42:07 +0900

    新たなスタイルを追加

--- lib/view/theme/my_text_style.dart
+++ lib/view/theme/my_text_style.dart
@@ -4,25 +4,40 @@ import 'package:kawarini_ketsudan/view/theme/my_color.dart';
 class MyTextStyle {
   static TextStyle _textStyle({
     Color? color,
-    required double fontSize,
+    double? fontSize,
     double? letterSpacing,
+    TextDecoration? decoration,
   }) {
     return TextStyle(
       color: color,
       fontSize: fontSize,
       fontWeight: FontWeight.bold,
       letterSpacing: letterSpacing,
+      decoration: decoration,
       overflow: TextOverflow.ellipsis,
     );
   }
 
   static TextStyle get black16 => _textStyle(fontSize: 16);
 
+  static TextStyle get white10 {
+    return _textStyle(color: MyColor.white, fontSize: 10);
+  }
+
+  static TextStyle get white => _textStyle(color: MyColor.white);
+
+  static TextStyle get whiteUnderline {
+    return _textStyle(
+      color: MyColor.white,
+      decoration: TextDecoration.underline,
+    );
+  }
+
   static TextStyle get white20 {
     return _textStyle(color: MyColor.white, fontSize: 20);
   }
 
-  static TextStyle get white10 {
-    return _textStyle(color: MyColor.white, fontSize: 10);
+  static TextStyle get white32LetterSpacing {
+    return _textStyle(color: MyColor.white, fontSize: 32, letterSpacing: 32);
   }
 }
