b489e3ac0d7e7e103d7371d4f23eec8242825c13
おりょう <oryo.developer@gmail.com>
2022-12-09 10:26:04 +0900

    テーマを更新

--- lib/view/theme/my_theme.dart
+++ lib/view/theme/my_theme.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:kawarini_ketsudan/view/theme/my_color.dart';
 
 class MyTheme {
-  static ThemeData get themeData => ThemeData();
+  static ThemeData get themeData {
+    return ThemeData(
+      iconTheme: const IconThemeData(color: MyColor.white),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          backgroundColor: MyColor.amber,
+          shadowColor: MyColor.transparent,
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          textStyle: const TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 32,
+          ),
+        ),
+      ),
+    );
+  }
 }
