c4481d976b77498e8c47ad04bb65d5966367c900
おりょう <oryo.developer@gmail.com>
2022-12-10 02:41:38 +0900

    新たなテーマを追加

--- lib/view/theme/my_theme.dart
+++ lib/view/theme/my_theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kawarini_ketsudan/view/theme/my_color.dart';
+import 'package:kawarini_ketsudan/view/theme/my_text_style.dart';
 
 class MyTheme {
   static ThemeData get themeData {
@@ -11,15 +12,22 @@ class MyTheme {
           backgroundColor: MyColor.amber,
           shadowColor: MyColor.transparent,
           padding: const EdgeInsets.symmetric(vertical: 8),
+          textStyle: MyTextStyle.white32LetterSpacing,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
           ),
-          textStyle: const TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 32,
-          ),
         ),
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          foregroundColor: MyColor.white,
+          textStyle: MyTextStyle.whiteUnderline,
+        ),
+      ),
+      inputDecorationTheme: const InputDecorationTheme(
+        border: InputBorder.none,
+        contentPadding: EdgeInsets.fromLTRB(16, 0, 0, 17),
+        suffixIconColor: MyColor.white,
       ),
     );
   }
