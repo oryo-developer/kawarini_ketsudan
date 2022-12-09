3ae849a1ee1c2d8914ecfaf863eac892e2976dd4
おりょう <oryo.developer@gmail.com>
2022-12-10 02:42:25 +0900

    新たな色を追加

--- lib/view/theme/my_color.dart
+++ lib/view/theme/my_color.dart
@@ -4,5 +4,6 @@ class MyColor {
   static const black = Colors.black;
   static const white = Colors.white;
   static const amber = Colors.amber;
+  static final darkAmber = Colors.amber[800];
   static const transparent = Colors.transparent;
 }
