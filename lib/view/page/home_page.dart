f098c1614aa3b075c2cb94babff8b1b549a42292
おりょう <oryo.developer@gmail.com>
2022-12-10 15:17:18 +0900

    ほぼ完成！(アニメーションなどは検討)

--- lib/view/page/home_page.dart
+++ lib/view/page/home_page.dart
@@ -2,140 +2,143 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:kawarini_ketsudan/controller/decisions_controller.dart';
 import 'package:kawarini_ketsudan/controller/home_page_controller.dart';
+import 'package:kawarini_ketsudan/controller/options_controller.dart';
 import 'package:kawarini_ketsudan/extension/text_editing_controller_extension.dart';
-import 'package:kawarini_ketsudan/model/option_model.dart';
 import 'package:kawarini_ketsudan/view/theme/my_color.dart';
 import 'package:kawarini_ketsudan/view/theme/my_text_style.dart';
 import 'package:marquee_widget/marquee_widget.dart';
-import 'package:uuid/uuid.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return HookConsumer(builder: (context, ref, child) {
-      final state = ref.watch(homePageProvider);
-      final controller = ref.read(homePageProvider.notifier);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(homePageProvider);
+    final controller = ref.read(homePageProvider.notifier);
 
-      return AnnotatedRegion(
-        value: const SystemUiOverlayStyle(
-          statusBarBrightness: Brightness.light,
-          statusBarIconBrightness: Brightness.light,
-        ),
-        child: Scaffold(
-          body: SafeArea(
-            minimum: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-            child: Column(children: [
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.only(
-                    bottom: state.bottomSheetVisible ? 66 : 16,
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: MyColor.amber, width: 2),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: state.optionsVisible ? _options() : _decisionHistory(),
+    return AnnotatedRegion(
+      value: const SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        body: SafeArea(
+          minimum: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+          child: Column(children: [
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(
+                  bottom: state.bottomSheetVisible ? 66 : 16,
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  border: Border.all(color: MyColor.amber, width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: state.optionsVisible ? _options() : _decisions(),
+              ),
+            ),
+            Visibility(
+              visible: !state.optionsVisible,
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => controller.show(),
+                  child: const Text('決断'),
                 ),
               ),
-              Visibility(
-                visible: !state.optionsVisible,
-                child: SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () => controller.show(options: []),
-                    child: const Text('決断'),
-                  ),
-                ),
-              ),
-            ]),
-          ),
-          bottomSheet: _bottomSheet(),
+            ),
+          ]),
         ),
-      );
-    });
+        bottomSheet: _bottomSheet(),
+      ),
+    );
   }
 
-  Widget _decisionHistory() {
-    return Column(children: [
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        child: Text('〜　今までの決断(10)　〜', style: MyTextStyle.black),
-      ),
-      Expanded(
-        child: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) => GestureDetector(
-            onTap: () {},
-            child: Container(
-              margin: const EdgeInsets.only(bottom: 16),
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: MyColor.amber,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(children: [
-                Row(children: [
-                  Expanded(child: Text('aiueo', style: MyTextStyle.white20)),
-                  _closeButton(onPressed: () {}),
-                ]),
-                const SizedBox(height: 4),
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: MyColor.white,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Marquee(
-                    pauseDuration: const Duration(seconds: 2),
-                    animationDuration: const Duration(seconds: 10),
-                    backDuration: const Duration(seconds: 10),
-                    forwardAnimation: Curves.easeInOut,
-                    backwardAnimation: Curves.easeInOut,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 8),
-                      child: Row(
-                        children: [
-                          Option(id: const Uuid().v4(), text: '付き合う'),
-                          Option(id: const Uuid().v4(), text: '付き合わない'),
-                          Option(id: const Uuid().v4(), text: '来年まで眠る'),
-                          Option(id: const Uuid().v4(), text: '再来年まで勉強する'),
-                          Option(id: const Uuid().v4(), text: '外を大晦日まで走ってくる'),
-                          Option(
-                              id: const Uuid().v4(),
-                              text: "I'm oryo developer."),
-                          Option(id: const Uuid().v4(), text: 'aiueoddd'),
-                          Option(id: const Uuid().v4(), text: 'あいうえおあいうえお'),
-                          Option(id: const Uuid().v4(), text: 'aiueo'),
-                          Option(
-                              id: const Uuid().v4(),
-                              text: 'あいうえおいうえおあうえおあいえおあいうおあいうえ'),
-                        ].map((option) {
-                          return _option(
-                            option: option,
-                            paddingRight: 8,
-                          );
-                        }).toList(),
+  Widget _decisions() {
+    return HookConsumer(builder: (context, ref, child) {
+      final controller = ref.read(homePageProvider.notifier);
+
+      final decisions = ref.watch(decisionsProvider);
+      final decisionsController = ref.read(decisionsProvider.notifier);
+
+      return Column(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: Text(
+            '〜　今までの決断(${decisions.length})　〜',
+            style: MyTextStyle.black,
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: decisions.length,
+            itemBuilder: (context, index) => GestureDetector(
+              onTap: () => controller.show(options: decisions[index].options),
+              child: Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: MyColor.amber,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(children: [
+                  Row(children: [
+                    Expanded(
+                      child: Text(
+                        decisions[index].option,
+                        style: MyTextStyle.white20,
+                      ),
+                    ),
+                    _closeButton(
+                      onPressed: () => decisionsController.delete(
+                        decision: decisions[index],
+                      ),
+                    ),
+                  ]),
+                  const SizedBox(height: 4),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: MyColor.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Marquee(
+                      pauseDuration: const Duration(seconds: 2),
+                      animationDuration: const Duration(seconds: 10),
+                      backDuration: const Duration(seconds: 10),
+                      forwardAnimation: Curves.easeInOut,
+                      backwardAnimation: Curves.easeInOut,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: Row(
+                          children: decisions[index].options.map(
+                            (option) {
+                              return _option(option: option, paddingRight: 8);
+                            },
+                          ).toList(),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ]),
+                ]),
+              ),
             ),
           ),
         ),
-      ),
-    ]);
+      ]);
+    });
   }
 
   Widget _options() {
     return HookConsumer(builder: (context, ref, child) {
       final state = ref.watch(homePageProvider);
-      final controller = ref.read(homePageProvider.notifier);
+
+      final options = ref.watch(optionsProvider);
+      final optionsController = ref.read(optionsProvider.notifier);
 
       return Center(
         child: SingleChildScrollView(
@@ -146,10 +149,10 @@ class HomePage extends StatelessWidget {
               alignment: WrapAlignment.center,
               spacing: 16,
               runSpacing: 16,
-              children: state.options.map((option) {
+              children: options.asMap().entries.map((entry) {
                 return _option(
-                  option: option,
-                  onPressed: () => controller.remove(option: option),
+                  option: entry.value,
+                  onPressed: () => optionsController.remove(index: entry.key),
                 );
               }).toList(),
             ),
@@ -160,7 +163,7 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _option({
-    required Option option,
+    required String option,
     VoidCallback? onPressed,
     double? paddingRight,
   }) {
@@ -178,7 +181,7 @@ class HomePage extends StatelessWidget {
               maxWidth: paddingRight == null ? double.infinity : 100,
             ),
             child: Text(
-              option.text,
+              option,
               style: paddingRight == null
                   ? MyTextStyle.whiteOverflow
                   : MyTextStyle.white10,
@@ -194,6 +197,8 @@ class HomePage extends StatelessWidget {
     return HookConsumer(builder: (context, ref, child) {
       final state = ref.watch(homePageProvider);
       final controller = ref.read(homePageProvider.notifier);
+
+      final options = ref.watch(optionsProvider);
 
       final textEditingController = useTextEditingController();
       final clearButtonVisible = useState(false);
@@ -251,7 +256,7 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               TextButton(
-                onPressed: 1 < state.options.length
+                onPressed: 1 < options.length
                     ? () {
                         final option = controller.decision();
                         showDialog(
@@ -270,7 +275,7 @@ class HomePage extends StatelessWidget {
     });
   }
 
-  Widget _dialog({required Option option}) {
+  Widget _dialog({required String option}) {
     return HookConsumer(builder: (context, ref, child) {
       final controller = ref.read(homePageProvider.notifier);
 
@@ -290,7 +295,7 @@ class HomePage extends StatelessWidget {
                   controller.hide();
                 },
                 child: const Text(
-                  'ホーム',
+                  'やめる',
                   style: TextStyle(color: MyColor.amber),
                 ),
               ),
